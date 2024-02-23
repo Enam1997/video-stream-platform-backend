64a6739fdeb84c19b7f03353972543945dc02691
Enam <mdenamahmedchowdhury@gmail.com>
2024-02-23 19:06:04 +0600

    Add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   loginUser,
   loggedOutUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUsser,
+  updateAccountDetials,
+  updateUserAvatar,
+  updateUserCover,
+  getUserChanelProfile,
+  getWatcHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -23,5 +30,18 @@ router.route("/login").post(loginUser);
 // Secure route
 router.route("/logout").post(verifyJWT, loggedOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUsser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetials);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCover);
+
+router.route("/c/:userName").get(verifyJWT, getUserChanelProfile);
+router.route("/history").get(verifyJWT, getWatcHistory);
 
 export default router;
